26ad1a80b1416fc11b00a659703e2fae9e745cfe
Daniel Henninger <daniel@ncsu.edu>
2017-02-17 21:14:42 -0500

    This should be a more proper test.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
-describe 'galera_arbitrator' do
 
-  context 'galera_nodes/galera_group set, defaults for rest of parameters' do
-    it { should contain_class('galera_arbitrator') }
+describe 'galera_arbitrator' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+      context 'galera_nodes/galera_group set, defaults for rest of parameters' do
+        let(:params) do
+          {
+            :galera_nodes => '1.1.1.1,2.2.2.2,3.3.3.3',
+            :galera_group => 'nonexistent_test_group',
+          }
+        end
+        it { is_expected.to contain_class('galera_arbitrator') }
+        it { is_expected.to contain_class('galera_arbitrator::install') }
+        it { is_expected.to contain_class('galera_arbitrator::config') }
+        it { is_expected.to contain_class('galera_arbitrator::service') }
+        case facts[:osfamily]
+        when 'RedHat'
+          it { is_expected.to contain_package('Percona-XtraDB-Cluster-garbd-5.7') }
+          it { is_expected.to contain_service('garb') }
+          it { is_expected.to contain_file('/etc/sysconfig/garb') }
+        when 'Debian'
+          it { is_expected.to contain_package('percona-xtradb-cluster-garbd-5.7') }
+          it { is_expected.to contain_service('garbd') }
+          it { is_expected.to contain_file('/etc/sysconfig/garbd') }
+        end 
+      end
+    end
   end
 end
