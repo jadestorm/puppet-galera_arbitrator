1e3b477a79c7af06ccdb7154ce8046a32df884da
Daniel Henninger <daniel@ncsu.edu>
2018-07-11 13:45:25 -0400

    Applied pdk template and fixed up some spec lint issues.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,13 +6,15 @@ describe 'galera_arbitrator' do
       let(:facts) do
         facts
       end
+
       context 'galera_nodes/galera_group set, defaults for rest of parameters' do
         let(:params) do
           {
-            :galera_nodes => '1.1.1.1,2.2.2.2,3.3.3.3',
-            :galera_group => 'nonexistent_test_group',
+            'galera_nodes' => '1.1.1.1,2.2.2.2,3.3.3.3',
+            'galera_group' => 'nonexistent_test_group',
           }
         end
+
         it { is_expected.to contain_class('galera_arbitrator') }
         it { is_expected.to contain_class('galera_arbitrator::install') }
         it { is_expected.to contain_class('galera_arbitrator::config') }
@@ -26,7 +28,7 @@ describe 'galera_arbitrator' do
           it { is_expected.to contain_package('percona-xtradb-cluster-garbd-5.7') }
           it { is_expected.to contain_service('garbd') }
           it { is_expected.to contain_file('/etc/default/garbd') }
-        end 
+        end
       end
     end
   end
