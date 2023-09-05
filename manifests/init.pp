4617a82f4543e20dda3b9437aafbe92fbe3e7f05
Daniel Henninger <daniel@ncsu.edu>
2023-09-05 16:09:54 -0400

    Very quick fix for issues with stdlib 9.  Will 2.0 this with a lot of QOL fixes ideally tomorrow.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,16 @@
 # == Class: galera_arbitrator
 class galera_arbitrator(
-  $servicename    = $galera_arbitrator::params::servicename,
-  $packagename    = $galera_arbitrator::params::packagename,
-  $sysconfig      = $galera_arbitrator::params::sysconfig,
-  $ensure_package = $galera_arbitrator::params::ensure_package,
-  $ensure_service = $galera_arbitrator::params::ensure_service,
-  $enable_service = $galera_arbitrator::params::enable_service,
-  $galera_nodes   = $galera_arbitrator::params::galera_nodes,
-  $galera_group   = $galera_arbitrator::params::galera_group,
-  $galera_options = $galera_arbitrator::params::galera_options,
-  $galera_logfile = $galera_arbitrator::params::galera_logfile,
+  String            $servicename    = $galera_arbitrator::params::servicename,
+  String            $packagename    = $galera_arbitrator::params::packagename,
+  String            $sysconfig      = $galera_arbitrator::params::sysconfig,
+  String            $ensure_package = $galera_arbitrator::params::ensure_package,
+  String            $ensure_service = $galera_arbitrator::params::ensure_service,
+  Boolean           $enable_service = $galera_arbitrator::params::enable_service,
+  Optional[String]  $galera_nodes   = $galera_arbitrator::params::galera_nodes,
+  Optional[String]  $galera_group   = $galera_arbitrator::params::galera_group,
+  Optional[String]  $galera_options = $galera_arbitrator::params::galera_options,
+  Optional[String]  $galera_logfile = $galera_arbitrator::params::galera_logfile,
 ) inherits galera_arbitrator::params {
-  validate_string($servicename)
-  validate_string($packagename)
-  validate_string($sysconfig)
-  validate_string($ensure_package)
-  validate_string($ensure_service)
-  validate_bool($enable_service)
-  if $galera_nodes {
-    validate_string($galera_nodes)
-  }
-  if $galera_group {
-    validate_string($galera_group)
-  }
-  if $galera_options {
-    validate_string($galera_options)
-  }
-  if $galera_logfile {
-    validate_string($galera_logfile)
-  }
-
   anchor{'galera_arbitrator::begin':}
   -> class{'::galera_arbitrator::install':}
   -> class{'::galera_arbitrator::config':}
